d75f0e4d5c8a8ef0f8b84c41c4c14f428903972d
Jen Solima <jensolima@gmail.com>
2016-03-29 10:49:39 -0500

    initial code for maintenence list population

--- app/controllers/VehMainCtrl.js
+++ app/controllers/VehMainCtrl.js
@@ -9,9 +9,18 @@ app.controller("VehMainCtrl",
   "firebaseURL",
   "MaintenanceFactory",
   "FirebaseFactory",
+  "VehicleFactory",
 
-  function ($scope, $location, $http, authFactory, firebaseURL, MaintenanceFactory) {
+  function ($scope, $location, $http, authFactory, firebaseURL, MaintenanceFactory, FirebaseFactory, VehicleFactory) {
+    
+    MaintenanceFactory().then(
+      maintenanceList => {
+        console.log("maintenanceList", maintenanceList);
 
+      function goToVehicle() {
+        console.log("test");
+      }
+    });
 
   }
 ]);

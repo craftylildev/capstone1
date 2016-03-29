f6e5c0c9eca01c369918169cf51855c38f4f6596
Jen Solima <jensolima@gmail.com>
2016-03-29 16:32:35 -0500

    maintenance schedule population complete

--- app/controllers/VehMainCtrl.js
+++ app/controllers/VehMainCtrl.js
@@ -13,14 +13,41 @@ app.controller("VehMainCtrl",
 
   function ($scope, $location, $http, authFactory, firebaseURL, MaintenanceFactory, FirebaseFactory, VehicleFactory) {
     
-    MaintenanceFactory().then(
-      maintenanceList => {
-        console.log("maintenanceList", maintenanceList);
+    $scope.vid = "";
 
-      function goToVehicle() {
-        console.log("test");
-      }
-    });
+    $scope.vehicleMainList = [];
+    $scope.mainItem = "";
+    $scope.mainAction = "";
+    $scope.intervalMileage = "";
 
-  }
-]);
+    $scope.goToVehicle = function (vehicleKey, car) {
+      $scope.car = car;
+      MaintenanceFactory(vehicleKey)
+      .then(
+        maintenanceList => {
+          console.log("maintenanceList", maintenanceList.actionHolder);
+          $scope.vehicleMainList = maintenanceList.actionHolder;
+        
+      }); 
+      populateVehicle();
+    } // end goToVehicle
+
+    function populateVehicle () {
+      FirebaseFactory()
+        .then(        
+          userVehicles => {
+            // console.log("userVehicles", userVehicles);
+            Object.keys(userVehicles).forEach(key => {
+              $scope.vid = userVehicles[key].id;
+              // console.log("vid", $scope.vid);
+              // $scope.vehicleMainList.push(userVehicles[key]);
+            });
+          }
+        )
+    } // end populateVehicle
+
+
+
+
+  } // end main function
+]); // end VehMainCtrl

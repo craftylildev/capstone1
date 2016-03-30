6e8c8d458241534d2fc9f8445eaaf7aa8de37978
Jen Solima <jensolima@gmail.com>
2016-03-30 14:46:56 -0500

    completed initial maintenance filter

--- app/controllers/VehMainCtrl.js
+++ app/controllers/VehMainCtrl.js
@@ -10,44 +10,47 @@ app.controller("VehMainCtrl",
   "MaintenanceFactory",
   "FirebaseFactory",
   "VehicleFactory",
+  "$route",
 
-  function ($scope, $location, $http, authFactory, firebaseURL, MaintenanceFactory, FirebaseFactory, VehicleFactory) {
+  function ($scope, $location, $http, authFactory, firebaseURL, MaintenanceFactory, FirebaseFactory, VehicleFactory, $route) {
     
-    $scope.vid = "";
-
     $scope.vehicleMainList = [];
-    $scope.mainItem = "";
-    $scope.mainAction = "";
-    $scope.intervalMileage = "";
+    $scope.vehicleSelected = false;
+    $scope.mileageFilter = {};
 
+    // display selected vehicle & service info
     $scope.goToVehicle = function (vehicleKey, car) {
       $scope.car = car;
       MaintenanceFactory(vehicleKey)
       .then(
         maintenanceList => {
-          console.log("maintenanceList", maintenanceList.actionHolder);
+          // console.log("maintenanceList", maintenanceList.actionHolder);
           $scope.vehicleMainList = maintenanceList.actionHolder;
-        
-      }); 
-      populateVehicle();
-    } // end goToVehicle
+      });
+      // set filter for mileage over 0
+      $scope.mileageOverZero($scope.vehicleMainList);
+      $scope.vehicleSelected = true; 
+    }; // end goToVehicle
+    
+    $scope.mileageOverZero = function(vehicleMainList) {
+      return vehicleMainList.intervalMileage > 0;
+    }
+   
+    $scope.clearVehicleInfo = function () {
+      $scope.vehicleSelected = false;
+    }
 
-    function populateVehicle () {
-      FirebaseFactory()
-        .then(        
+    // delete a selected vehicle
+    $scope.vehicleDelete = (car) => { 
+      // console.log("car", car);
+      FirebaseFactory(car.vehicleKey)
+        .then(
           userVehicles => {
-            // console.log("userVehicles", userVehicles);
-            Object.keys(userVehicles).forEach(key => {
-              $scope.vid = userVehicles[key].id;
-              // console.log("vid", $scope.vid);
-              // $scope.vehicleMainList.push(userVehicles[key]);
-            });
-          }
-        )
-    } // end populateVehicle
-
-
-
+            $http.delete(`${firebaseURL}/vehicle/${car.id}.json`);
+            $route.reload();
+          })
+      $scope.vehicleSelected = false; 
+    }; // end vehicleDelete
 
   } // end main function
 ]); // end VehMainCtrl
